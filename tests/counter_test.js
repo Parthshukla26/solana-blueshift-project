d2c78d8bba266680a50131ca064239996cc33709
Parth Shukla <parthshukla808@gmail.com>
2025-09-09 00:25:23 +0530

    Add Anchor JS test for counter program

---
+++ tests/counter_test.js
@@ -0,0 +1,44 @@
+const anchor = require("@coral-xyz/anchor");
+const { SystemProgram } = anchor.web3;
+
+describe("hello_solana counter test", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.HelloSolana;
+
+  let counterPda;
+
+  it("Initializes a counter", async () => {
+    const [pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("counter"), provider.wallet.publicKey.toBuffer()],
+      program.programId
+    );
+    counterPda = pda;
+
+    await program.methods
+      .initCounter()
+      .accounts({
+        authority: provider.wallet.publicKey,
+        counter: counterPda,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+
+    const account = await program.account.counter.fetch(counterPda);
+    console.log("Counter initialized:", account.count.toString());
+  });
+
+  it("Increments the counter", async () => {
+    await program.methods
+      .increment()
+      .accounts({
+        authority: provider.wallet.publicKey,
+        counter: counterPda,
+      })
+      .rpc();
+
+    const account = await program.account.counter.fetch(counterPda);
+    console.log("Counter after increment:", account.count.toString());
+  });
+});
